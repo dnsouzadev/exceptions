94318092db2f3c6e7fe920b4f1869e908523933c
Daniel Souza <workdndsza@gmail.com>
2023-09-22 21:17:38 -0300

    creating the program class

--- exceptions1/exceptions-1/src/model/entities/Reservation.java
+++ exceptions1/exceptions-1/src/model/entities/Reservation.java
@@ -4,7 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
+
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
@@ -12,6 +15,9 @@ public class Reservation {
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -29,7 +35,6 @@ public class Reservation {
         return checkIn;
     }
 
-
     public Date getCheckOut() {
         return checkOut;
     }
@@ -40,6 +45,13 @@ public class Reservation {
     }
 
     public void updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
@@ -56,5 +68,4 @@ public class Reservation {
                 + duration()
                 + " nights";
     }
-
 }
